08fe25222b206336d90853db6c986d00654c10be
MayankN24 <mayankku24@gmail.com>
2024-09-11 17:56:52 +0530

    random

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true; //playerX, playerO
+let turnO = true; //playerX, playerO turn
 
 const winPatterns = [
   [0,1,2],
